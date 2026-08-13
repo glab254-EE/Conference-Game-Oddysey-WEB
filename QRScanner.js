30821fd64080b7aaf3337fd941d318c0ed8daee5
glab254-EE <glebvarlam@gmail.com>
2026-08-13 20:11:09 +0500

    Bug Fixes

--- QRScanner.js
+++ QRScanner.js
@@ -1,29 +1,37 @@
 let qrScanner = null;
 let scannerRunning = false;
 
-function startScanner() {
-    const reader = document.getElementById("reader");
-    reader.style.display="block";
-    qrScanner = new Html5Qrcode("reader");
-    qrScanner.start(
-        {
-            facingMode: "environment"
-        },
-        {
-            fps: 10,
-            qrbox: 250
-        },
-        function(decodedText) {
+function onScanSuccess(decodedText, decodedResult) {
+    var resT = JSON.parse(decodedText)
+    if (resT["IsleKey"] && resT["IsleName"])
+    {
 			unityInstance.SendMessage(
 				"QRScanner",
 				"OnQRCodeDetected",
 				decodedText
-			);
-        },
-        function(errorMessage) {
-        }
-    );
-    scannerRunning = true; 
+			);      
+    }
+}
+function startScanner() {
+    const reader = document.getElementById("reader");
+    reader.style.visibility="visible";
+    qrScanner = new Html5Qrcode("reader");
+    qrScanner.start(
+    { facingMode: "environment" }, 
+    {
+        fps: 10,    // Optional, frame per seconds for qr code scanning
+        qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
+    },
+    (decodedText, decodedResult) => {
+        onScanSuccess(decodedText,decodedResult);
+    },
+    (errorMessage) => {
+        // parse error, ignore it.
+    })
+    .catch((err) => {
+    // Start failed, handle it.
+});
+scannerRunning = true;
 }
 
 async function stopScanner() {
@@ -31,7 +39,7 @@ async function stopScanner() {
         return;
     await qrScanner.stop();
     await qrScanner.clear();
-    document.getElementById("reader").styleяdisplay="none";
+    document.getElementById("reader").visibility="hidden";
     qrScanner = null;
     scannerRunning = false;
 }
