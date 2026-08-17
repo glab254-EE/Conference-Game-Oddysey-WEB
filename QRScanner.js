c291f5570be83ef3f66e9f9c7321570d3a3d1794
glab254-EE <glebvarlam@gmail.com>
2026-08-17 16:47:46 +0500

    Fixes

--- QRScanner.js
+++ QRScanner.js
@@ -1,17 +1,6 @@
 let qrScanner = null;
 let scannerRunning = false;
 
-function onScanSuccess(decodedText, decodedResult) {
-    var resT = JSON.parse(decodedText)
-    if (resT["IsleKey"] && resT["IsleName"])
-    {
-			unityInstance.SendMessage(
-				"QRScanner",
-				"OnQRCodeDetected",
-				decodedText
-			);      
-    }
-}
 function startScanner() {
     const reader = document.getElementById("reader");
     reader.style.visibility="visible";
@@ -20,18 +9,21 @@ function startScanner() {
     { facingMode: "environment" }, 
     {
         fps: 10,    // Optional, frame per seconds for qr code scanning
-        qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
     },
     (decodedText, decodedResult) => {
-        onScanSuccess(decodedText,decodedResult);
+		unityInstance.SendMessage(
+			"QRScanner",
+			"OnQRCodeDetected",
+			decodedText
+		);   
     },
     (errorMessage) => {
         // parse error, ignore it.
     })
     .catch((err) => {
     // Start failed, handle it.
-});
-scannerRunning = true;
+	});
+	scannerRunning = true;
 }
 
 async function stopScanner() {
